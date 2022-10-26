37c1cf3795999c05760c8bd2519918e75a505f84
Logan <cummingslogant@gmail.com>
2022-10-25 23:32:21 -0500

    initial commit 2.0

---
+++ src/main/java/com/Hackbright/JavaCapstone/JavaCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.Hackbright.JavaCapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Hackbright/JavaCapstone/JavaCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hackbright.JavaCapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
